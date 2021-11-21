fe16c69d0a3150fe99c10d7a736e4c37b9f2d433
unknown <gonzales.louie12301@gmail.com>
2021-11-20 17:34:25 -0800

    first commit

---
+++ number.py
@@ -0,0 +1,11 @@
+a = int(input("Enter first number :"))
+b = int(input("Enter second number :"))
+c = int(input("Enter third number :"))
+
+smallest=0
+
+if a<b and a<c: smallest=a
+if b<a and b<c: smallest=b
+if c<a and c<b: smallest=c
+
+print(smallest,"is the smallest of three numbers.")
